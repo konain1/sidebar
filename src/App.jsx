9e94f4353c9f3d47f413b57bcba9487cf515ee4a
Konain1 <kaunainkasab@gmail.com>
2024-05-15 20:14:39 +0530

    nocode

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+
+import Sidebar from './component/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+ <div className='bg-[black] h-screen'>
 
-      
+ <Sidebar></Sidebar>  
+    </div>
     </>
+   
   )
 }
 
---
+++ src/component/Sidebar.jsx
@@ -0,0 +1,25 @@
+import React, { Children } from 'react'
+import {ChevronFirst} from "lucide-react"
+import { Camera } from 'lucide-react';
+
+function Sidebar({Children}) {
+  return (
+    <aside className='h-screen'>
+ 
+    <nav className='h-full flex flex-col bg-[lightgreen]  border-r shadow-sm'>
+        <div className='p-4 pb-2 flex justify-between items-center border border-black'>
+            <img className='h-[30px] w-[32px]' src=' https://i.pinimg.com/originals/b8/fd/82/b8fd821c02183a8d19765c9362bf2465.jpg' />
+
+            <button className='flex'>
+                <ChevronFirst />
+             </button>
+        </div>
+        <ul className='flex-1 px-3'> {Children}</ul>
+    </nav>
+    </aside>
+  )
+}
+
+
+
+export default Sidebar
